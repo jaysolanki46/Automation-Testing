b9b1c74842635f70b3e23cd086de27c54a55f562
jay solanki <jaysolanki46@gmail.com>
2020-06-30 21:26:19 +1200

    Introduced/Init Gmail account login automation

--- src/gmail/Gmail.java
+++ src/gmail/Gmail.java
@@ -3,13 +3,35 @@
  */
 package gmail;
 
+import java.awt.Robot;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 /**
  * @author jayso
  *
  */
 public class Gmail {
 
-	public void login() {
-		System.out.println("Hello world, this is automation login test");
+	private WebDriver driver;
+	
+	public Gmail(String email, String pass) {
+		// Initialized drivers & libraries
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\libs\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://accounts.google.com/signin");
+		
+		driver.findElement(By.id("identifierId")).sendKeys(email);
+		driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();        
+
+		// Password field coming soon .........
+		
 	}
 }
--- src/init/InitMain.java
+++ src/init/InitMain.java
@@ -3,13 +3,16 @@ package init;
 import java.awt.AWTException;
 
 import crowdpie.CrowdPie;
+import gmail.Gmail;
 
 public class InitMain {
 	
 	public static void main(String[] args) throws InterruptedException, AWTException {
 		
-		// www.crowdpie.com
+		// Test case 1: www.crowdpie.com
 		new CrowdPie("Git@test.com", "MyPassword", 998877);
 		
+		// Test case 2: www.gmail.com
+		new Gmail("gmail@test.com", "MyPassword");
 	}
 }
