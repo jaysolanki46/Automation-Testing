57e7935326657d2e39764d9fe52234f728ccab59
jay solanki <jaysolanki46@gmail.com>
2020-09-03 23:19:17 +1200

    Setup .exe file

--- src/fb/Facebook.java
+++ src/fb/Facebook.java
@@ -3,14 +3,20 @@
  */
 package fb;
 
+import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;import org.openqa.selenium.interactions.Keyboard;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -21,11 +27,22 @@ public class Facebook {
 
 	private WebDriver driver;
 	
-	public Facebook(String email, String pass) {
+	public Facebook(String email, String pass) throws AWTException, InterruptedException {
+		
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("credentials_enable_service", false);
+		prefs.put("profile.password_manager_enabled", false);
+		
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+		options.addArguments("start-maximized", "--start-maximized", "--disable-web-security", "--no-proxy-server");
+		options.setExperimentalOption("useAutomationExtension", false);
+		options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+		
 		// Initialized drivers & libraries
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\libs\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		driver = new ChromeDriver(options);
+		
 		driver.get("https://www.facebook.com/");
 		
 		driver.findElement(By.name("email")).sendKeys(email);
@@ -33,5 +50,8 @@ public class Facebook {
 		
 		driver.findElement(By.name("login")).click();
 		
+		Thread.sleep(10000);
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_ESCAPE);
 	}
 }
--- src/init/InitMain.java
+++ src/init/InitMain.java
@@ -1,20 +1,32 @@
 package init;
 
 import java.awt.AWTException;
-
-import crowdpie.CrowdPie;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import fb.Facebook;
-import gmail.Gmail;
 
 public class InitMain {
 	
-	public static void main(String[] args) throws InterruptedException, AWTException {
+	public static void main(String[] args) throws InterruptedException, AWTException, FileNotFoundException {
+		
+		String credentialsDetails = "";
+		File file = new File(System.getProperty("user.dir") + "\\credentials.txt"); 
+		Scanner sc = new Scanner(file); 
+
+		while (sc.hasNextLine()) 
+		     credentialsDetails = sc.nextLine(); 
+		
+		String[] currencies = credentialsDetails.split(";");
+		String username = currencies[0];
+		String pass = currencies[1];
+		System.err.println(username + "=" + pass);
 		
 		// Test case 1: www.crowdpie.com
 		//new CrowdPie("Git@test.com", "MyPassword", 998877);
 		
 		// Test case 2: www.facebook.com
-		new Facebook("######", "********");
+		new Facebook(username, pass);
 		
 		// Test case 3: www.gmail.com
 		//new Gmail("jaysolanki46@gmail.com", "");
